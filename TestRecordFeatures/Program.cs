c0e78df352b42dc67180e654ac8b19a8502a9345
Nikita B. Zuev <nikitazu@gmail.com>
2021-10-27 00:48:41 +0300

    Add deconstruct example

--- TestRecordFeatures/Program.cs
+++ TestRecordFeatures/Program.cs
@@ -8,7 +8,9 @@ namespace TestRecordFeatures
         {
             var pc = new PersonClass("Senchou", 17);
             var pr = new PersonRecord("Sonchou", 100500);
+            var par = new PersonAntiRecord("Tenchou", 666);
 
+            // init setter example
 
             var x = new PersonRecord("Name1", 18)
             {
@@ -16,8 +18,19 @@ namespace TestRecordFeatures
             };
             Console.WriteLine($"X {x}");
 
+            // to string example
+
             Console.WriteLine($"PC {pc}");
             Console.WriteLine($"PR {pr}");
+            Console.WriteLine($"PAR {par}");
+
+            // deconstruction example
+
+            var (recordName, _) = pr;
+            var (antiRecordName, _) = par;
+
+            Console.WriteLine($"Record name is {recordName}");
+            Console.WriteLine($"Anti-Record name is {antiRecordName}");
         }
     }
 }
